a33b4df84194b1b2704d7b06f8c96d73f017904a
龙哥 <15966960596@163.com>
2015-05-05 23:51:19 +0800

    初始化项目

---
+++ tools/tools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tools
+//
+//  Created by long108 on 15/5/5.
+//  Copyright (c) 2015年 long108. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tools/tools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tools
+//
+//  Created by long108 on 15/5/5.
+//  Copyright (c) 2015年 long108. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
